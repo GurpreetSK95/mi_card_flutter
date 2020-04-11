0f7f4311661d5e2531f8ca515adf6a97c742a2a3
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-04-12 00:32:10 +0530

    Create business card.

--- lib/main.dart
+++ lib/main.dart
@@ -11,52 +11,98 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: BodyWidget(),
-          left: false,
-          right: false,
+          child: BusinessCard(),
         ),
       ),
     );
   }
 }
 
-class BodyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: <Widget>[
-        Container(
-          color: Colors.red,
-          width: 100.0,
-        ),
-        SquareWidgetWithShadow(),
-        Container(
-          color: Colors.blue,
-          width: 100.0,
-        ),
-      ],
-    );
-  }
-}
-
-class SquareWidgetWithShadow extends StatelessWidget {
+class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Container(
-          width: 100.0,
-          height: 100.0,
-          color: Colors.yellow,
+        CircleAvatar(
+          radius: 50.0,
+          backgroundImage: AssetImage(
+            "images/DP.png",
+          ),
         ),
-        Container(
-          width: 100.0,
-          height: 100.0,
-          color: Colors.yellow[300].withAlpha(70),
-        )
+        Text(
+          "Gurpreet Singh",
+          style: TextStyle(
+              color: Colors.white, fontSize: 30.0, fontFamily: "Pacifico"),
+        ),
+        Text(
+          "MOBILE DEVELOPER",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15.0,
+            fontWeight: FontWeight.w300,
+            letterSpacing: 1.5,
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+          child: Divider(
+            color: Colors.teal.shade100,
+            indent: 80.0,
+            endIndent: 80.0,
+          ),
+        ),
+        CallContactCard(),
+        MailContactCard()
       ],
+    );
+  }
+}
+
+class MailContactCard extends StatelessWidget {
+  const MailContactCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      margin: EdgeInsets.symmetric(
+        horizontal: 40.0,
+        vertical: 10.0,
+      ),
+      child: ListTile(
+        title: Text("gurpreetsk1@gmail.com"),
+        leading: Icon(
+          Icons.mail,
+          color: Colors.teal,
+        ),
+      ),
+    );
+  }
+}
+
+class CallContactCard extends StatelessWidget {
+  const CallContactCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(
+        horizontal: 40.0,
+        vertical: 10.0,
+      ),
+      child: ListTile(
+          leading: Icon(
+            Icons.call,
+            color: Colors.teal,
+          ),
+          title: Text(
+            "+91-9971897557",
+          )),
     );
   }
 }
