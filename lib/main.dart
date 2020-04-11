6085a494b50660da4843899210e27fff3738b095
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-04-11 23:47:35 +0530

    Complete challenge to make UI composing rows and columns.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: BodyWidget(),
+          left: false,
+          right: false,
+        ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+          width: 100.0,
+        ),
+        SquareWidgetWithShadow(),
+        Container(
+          color: Colors.blue,
+          width: 100.0,
+        ),
+      ],
+    );
+  }
+}
+
+class SquareWidgetWithShadow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 100.0,
+          height: 100.0,
+          color: Colors.yellow,
+        ),
+        Container(
+          width: 100.0,
+          height: 100.0,
+          color: Colors.yellow[300].withAlpha(70),
+        )
+      ],
+    );
+  }
 }
